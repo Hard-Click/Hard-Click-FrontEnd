1a3ad93b7371138f683c48d74fc04518651fa495
Siyun.Gwak <geunji04@g.eulji.ac.kr>
2026-05-22 12:34:33 +0900

    [FEAT]로그인 유효성 검사 및 에러메세지 구현 #7 #12

--- hard-click-frontend/src/features/auth/components/PasswordInput.tsx
+++ hard-click-frontend/src/features/auth/components/PasswordInput.tsx
@@ -1,54 +1,64 @@
 'use client';
 
 import Image from 'next/image';
-import { useState } from 'react';
+import { forwardRef, useState } from 'react';
+
+import LoginErrorMessage from './LoginErrorMessage';
 
 interface PasswordInputProps {
   value: string;
   onChange: (value: string) => void;
+  error?: string;
 }
 
-export default function PasswordInput({ value, onChange }: PasswordInputProps) {
-  const [showPassword, setShowPassword] = useState(false);
+const PasswordInput = forwardRef<HTMLInputElement, PasswordInputProps>(
+  ({ value, onChange, error }, ref) => {
+    const [showPassword, setShowPassword] = useState(false);
 
-  return (
-    <div>
-      <label className="mb-3 block text-lg font-semibold text-[#1F2937]">
-        비밀번호
-      </label>
+    return (
+      <div>
+        <label className="mb-3 block text-lg font-semibold text-[#1F2937]">
+          비밀번호
+        </label>
 
-      <div className="flex h-16 items-center rounded-2xl border border-[#E2E8F0] px-5">
-        {/* lock icon */}
-        <Image
-          src="/icons/passwordIcon.svg"
-          alt="password"
-          width={20}
-          height={20}
-        />
-
-        {/* input */}
-        <input
-          type={showPassword ? 'text' : 'password'}
-          placeholder="비밀번호를 입력하세요"
-          value={value}
-          onChange={(e) => onChange(e.target.value)}
-          className="ml-4 w-full bg-transparent text-lg outline-none placeholder:text-[#9CA3AF]"
-        />
-
-        {/* eye button */}
-        <button
-          type="button"
-          onClick={() => setShowPassword((prev) => !prev)}
-          className="flex items-center justify-center"
+        <div
+          className={`flex h-16 items-center rounded-2xl border px-5 border-[#E2E8F0] focus-within:border-[#B91C1C] focus-within:border-[#B91C1C] `}
         >
           <Image
-            src={showPassword ? '/icons/openEye.svg' : '/icons/closeEye.svg'}
-            alt="toggle password"
-            width={22}
-            height={22}
+            src="/icons/passwordIcon.svg"
+            alt="password"
+            width={20}
+            height={20}
           />
-        </button>
+
+          <input
+            ref={ref}
+            type={showPassword ? 'text' : 'password'}
+            placeholder="비밀번호를 입력하세요"
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+            className="ml-4 w-full bg-transparent text-lg outline-none placeholder:text-[#9CA3AF]"
+          />
+
+          <button
+            type="button"
+            onClick={() => setShowPassword((prev) => !prev)}
+            className="flex items-center justify-center"
+          >
+            <Image
+              src={showPassword ? '/icons/openEye.svg' : '/icons/closeEye.svg'}
+              alt="toggle password"
+              width={28}
+              height={28}
+            />
+          </button>
+        </div>
+
+        <LoginErrorMessage message={error} />
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
+PasswordInput.displayName = 'PasswordInput';
+
+export default PasswordInput;
