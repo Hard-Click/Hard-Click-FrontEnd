d8d9ef87067530e3408d8ea74572422bf1c6a03b
Hyun An <hyun030514@g.eulji.ac.kr>
2026-07-02 00:50:18 +0900

    [CHORE] ReviewFormModal 미사용 toast import 제거 #707
    - import 전환 과정에서 드러난, 원래부터 사용되지 않던 toast import 정리(내가 만진 파일 lint clean 유지)

--- hard-click-frontend/src/features/reviews/components/ReviewFormModal.tsx
+++ hard-click-frontend/src/features/reviews/components/ReviewFormModal.tsx
@@ -2,7 +2,6 @@
 
 import { useRef, useState } from 'react';
 import Image from 'next/image';
-import { toast } from '@/lib/toast';
 import LoadingModal from '@/components/ui/loadingModal';
 
 interface ReviewFormModalProps {
